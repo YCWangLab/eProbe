a704702eccababf50a900d9751fb9c6f87c5abae
Zi-Hao Huang <134923928+BioJoe-Hzh@users.noreply.github.com>
2024-11-04 10:24:08 +0000

    Update Get_probe_from_allele.py
    better handle complex indels

--- eModules/Get_probe_from_allele.py
+++ eModules/Get_probe_from_allele.py
@@ -1,180 +1,215 @@
-from Bio.Align.Applications import ClustalwCommandline
-from Bio import AlignIO
+import pybedtools
+import pysam
+import subprocess
+import multiprocessing
 import os
+import shutil
 import datetime
-from fasta_operator import read_fasta, write_fasta
+from pipe_controler import make_temp_dir
+from fasta_operator import write_fasta
 
 
-def multiple_sequence_alignment(sequences, temp_base):
-    input_file = f'{temp_base}.temp.fasta'
-    output_file = f'{temp_base}.temp.aln'
-    temp_file = f'{temp_base}.temp.dnd'
-    write_fasta(sequences, input_file)
+# phase an interval (i.e., a BED object) of a whole VCF
+def phase_common(bed_obj, vcf_path, tmp_dir, name):
+    chrom = bed_obj.chrom
+    start = bed_obj.start
+    end = bed_obj.end
 
-    # run clustalw
-    clustalw_cline = ClustalwCommandline('clustalw2', infile=input_file)
-    stdout, stderr = clustalw_cline()
+    output_bcf = os.path.join(tmp_dir, f'{name}.bcf')
+    output_vcf = os.path.join(tmp_dir, f'{name}.vcf')
+    output_vcf_gz = f'{output_vcf}.gz'
 
-    if os.path.exists(output_file):
-        alignment = AlignIO.read(output_file, 'clustal')
+    success = True
+
+    if not os.path.exists(output_bcf):
+        try:
+            shapeit_command = f'phase_common --input {vcf_path} --output-format bcf --output {output_bcf} --region {chrom}:{start}-{end} --thread 1'
+            subprocess.run(shapeit_command, shell=True, check=True)
+
+            bcftools_command = f'bcftools view {output_bcf} -Ov -o {output_vcf}'
+            subprocess.run(bcftools_command, shell=True, check=True)
+
+            bgzip_command = f'bgzip -c {output_vcf} > {output_vcf_gz}'
+            subprocess.run(bgzip_command, shell=True, check=True)
+
+            tabix_command = f'tabix -f {output_vcf_gz}'
+            subprocess.run(tabix_command, shell=True, check=True)
+        except subprocess.CalledProcessError:
+            print(f"Error occurred during phasing in region {chrom}:{start}-{end}. "
+                  f"Possibly because no variants in this regions. Skipping...")
+            success = False
     else:
-        raise FileNotFoundError(f'ClustalW failed to produce output file: {output_file}')
+        print(f'Duplicated genomic region detected: {name}. Skipping phasing...')
 
-    # remove temp
-    os.remove(input_file)
-    os.remove(output_file)
-    os.remove(temp_file)
-
-    return alignment
+    return 1 if success else 0
 
 
-def sliding_window_variation(alignment, window_size=100, step_size=30):
-    alignment_length = alignment.get_alignment_length()
-    variations = []
+def replace_dna_sequence(template, pos_r, pos_snp, ref, allele):
+    # pos_r: relative position within bed; pos_snp: genome position
+    ref_len = len(ref)
+    allele_len = len(allele)
 
-    for start in range(0, alignment_length - window_size + 1, step_size):
-        end = start + window_size
-        window = alignment[:, start:end]
-        variation_count = 0
-        for i in range(end - start):
-            column = window[:, i]
-            if len(set(column)) > 1:  # check difference (mutation)
-                variation_count += 1
+    # Handle substitution
+    if ref_len == allele_len == 1:
+        template[pos_r] = allele
 
-        variations.append((start, end, variation_count, window))
+    # Handle deletion (ref is longer than allele)
+    elif ref_len > allele_len:
+        start = pos_r
+        end = pos_r + ref_len
+        template[start:end] = [''] * (end - start) 
+        template[pos_r] = allele 
 
-    return variations
+    # Handle insertion (allele is longer than ref)
+    elif allele_len > ref_len:
+        start = pos_r
+        end = pos_r + ref_len
+        template[start:end] = [''] * (end - start)
+        template.insert(pos_r, allele)
 
+    else:
+        print(f"Unhandled case at position {pos_snp}: ref='{ref}' allele='{allele}'")
 
-def find_original_position(aligned_seq, original_seq):
-    # remove gap '-' and upper the seq
-    trimmed_seq = aligned_seq.replace('-', '').upper()
-
-    # upper the seq before searching
-    original_position = original_seq.upper().find(trimmed_seq)
-
-    return original_position
-
-
-def get_probes_with_seq(seq, length, step):
-    probes_list = []
-    i = 0
-    if len(seq) > length:
-        while True:
-            if i + int(length) < len(seq):
-                probe = seq[i:i + int(length)]
-                if len(probe) == length:
-                    probes_list.append(probe)
-                i = i + int(step)
-            else:
-                probe = seq[-int(length):]
-                if len(probe) == length:
-                    probes_list.append(probe)
-                break
-    return probes_list
+    return template
 
 
-def get_probes_with_alleles(alleles_dict, temp_base, window_size, step_size):
-    probe_seq = {}
+def calculate_frequencies(input_list):
+    frequency_dict = {}
+    total_count = len(input_list)
 
-    for gene, alleles in alleles_dict.items():
-        if gene not in probe_seq.keys():
-            probe_seq[gene] = []
+    for item in input_list:
+        frequency_dict[item] = frequency_dict.get(item, 0) + 1
 
-        allele_seqs = {}
-        for num, allele_seq in enumerate(alleles.values()):
-            allele_seqs[str(num)] = allele_seq
-        # have alleles
-        if len(allele_seqs) > 1:
-            # align different alleles
-            alignment = multiple_sequence_alignment(allele_seqs, temp_base)
-            # detect variation from alignment
-            window_alignments = sliding_window_variation(alignment, window_size, step_size)
-            for start, end, variation_count, window_alignment in window_alignments:
-                # no variation
-                if int(variation_count) == 0:
-                    probe_seq[gene].append(str(window_alignment[0].seq))
-                # have variation
-                else:
-                    alleles_in_window = []
-                    for record in window_alignment:
-                        start = int(find_original_position(str(record.seq), str(allele_seqs[record.id])))
-                        end = start + window_size
-                        alleles_in_window.append(str(allele_seqs[record.id])[start:end])
-                    probe_seq[gene] = probe_seq[gene] + list(set(alleles_in_window))
+    for item in frequency_dict:
+        frequency_dict[item] /= total_count
 
-        else:
-            probe_seq[gene] = get_probes_with_seq(list(allele_seqs.values())[0], window_size, step_size)
-
-    return probe_seq
+    return frequency_dict
 
 
-def process_fasta(fasta_dict, method, separator, min_freq):
-    alleles_dict = {}
+def get_bed_sequence(chrom, start, end, fasta_obj):
+    sequence = fasta_obj.fetch(chrom, start, end)
+    return sequence
 
-    def add_to_dict(gene_id, allele_num, seq):
-        if gene_id not in alleles_dict:
-            alleles_dict[gene_id] = {}
-        alleles_dict[gene_id][allele_num] = seq
 
-    for header, seq in fasta_dict.items():
-        if method.lower() == 'bed':
-            gene_id = header.split('_Allele')[0]
-            allele_num = header.split('_Allele')[1].split('_Freq')[0]
-            freq = header.split('_Freq')[1]
-            if float(freq) >= float(min_freq):
-                add_to_dict(gene_id, allele_num, seq)
-        elif method.lower() == 'seq':
-            gene_id, allele_num = header.split(separator)[:2]
-            add_to_dict(gene_id, allele_num, seq)
+def get_allele_sequences(bed_obj, fasta_obj, bed_vcf):
+    vcf_reader = pysam.VariantFile(bed_vcf)
 
-    return alleles_dict
+    bed_chrom = bed_obj.chrom
+    bed_start = bed_obj.start
+    bed_end = bed_obj.end
+    ref_sequence = get_bed_sequence(bed_chrom, bed_start - 1, bed_end, fasta_obj) # use 0-based position
+
+    sample_list = list(vcf_reader.header.samples)
+    sample_variants = {sample: {} for sample in sample_list} 
+    sample_sequences = {sample: {0: list(ref_sequence), 1: list(ref_sequence)} for sample in sample_list}
+
+    for record in vcf_reader.fetch(bed_chrom, bed_start, bed_end + 1):
+        pos_snp = record.pos
+        # relative position in the interval
+        pos_r = pos_snp - bed_start # 0-based position
+
+        ref = record.ref
+        alts = record.alts
+        alleles = [ref] + list(alts)
+
+        for sample in record.samples:
+            GT1 = record.samples[sample]['GT'][0]
+            GT2 = record.samples[sample]['GT'][1]
+            if GT1 is not None and GT2 is not None:
+                sample_variants[sample][f'{pos_r}_{pos_snp}'] = {'ref': ref, 0: alleles[GT1], 1: alleles[GT2]}
+
+    for sample, variants in sample_variants.items():
+        for pos, variant in variants.items():
+            # for haplotype 0
+            if variant['ref'] != variant[0]:
+                sample_sequences[sample][0] = replace_dna_sequence(sample_sequences[sample][0], int(pos.split("_")[0]), int(pos.split("_")[1]),variant['ref'], variant[0])
+            # for haplotype 1
+            if variant['ref'] != variant[1]:
+                sample_sequences[sample][1] = replace_dna_sequence(sample_sequences[sample][1], int(pos.split("_")[0]), int(pos.split("_")[1]),variant['ref'], variant[1])
+
+    sample_sequences = {
+        sample: {0: ''.join(haplotypes[0]), 1: ''.join(haplotypes[1])}
+        for sample, haplotypes in sample_sequences.items()
+    }
+
+    bed_alleles = calculate_frequencies(
+        [sample_allele for sample in sample_sequences.values() for sample_allele in sample.values()]
+    )
+
+    return bed_alleles
+
+
+def lanch_process(bed_index, bed_file, whole_vcf, genome_file, tmp_dir):
+    fasta_obj = pysam.FastaFile(genome_file)
+    BED = pybedtools.BedTool(bed_file)
+    bed_obj = BED[bed_index]
+    # name of the interval
+    if len(bed_obj.name) <= 1:
+        bed_obj_name = f'{bed_obj.chrom}:{bed_obj.start}-{bed_obj.end}'
+    else:
+        bed_obj_name = bed_obj.name
+
+    # has variants in this region?
+    v = phase_common(bed_obj, whole_vcf, tmp_dir, bed_obj_name)
+    if v == 1:
+        bed_vcf = os.path.join(tmp_dir, f'{bed_obj_name}.vcf.gz')
+        bed_alleles = get_allele_sequences(bed_obj, fasta_obj, bed_vcf)
+    else:
+        bed_chrom = bed_obj.chrom
+        bed_start = bed_obj.start - 1
+        bed_end = bed_obj.end - 1
+        ref_sequence = get_bed_sequence(bed_chrom, bed_start, bed_end, fasta_obj)
+        bed_alleles = calculate_frequencies([ref_sequence])
+    return bed_obj_name, bed_alleles
 
 
 if __name__ == '__main__':
     import argparse
 
-    usage = '''python Get_probe_from_allele.py -f *.fasta -l 100 -s 30 -o output.fasta'''
+    usage = '''python Get_allele_from_vcf.py -v vcf.gz -r genome.fasta -b genome_block.bed -t threads -o output.fasta'''
 
     parser = argparse.ArgumentParser(description=usage)
-    parser.add_argument('--input', dest='input', action='store', nargs='?',
-                        help='Source of input data (seq/bed, default: bed).'
-                             'seq: allele sequences collected by user ("seq" Method in Seq generator).'
-                             'bed: allele sequences inferred by eProbe ("bed" Method in Seq generator).',
-                        choices=['seq', 'bed'], default='bed', metavar='STRING')
-    parser.add_argument('-f', '--fasta', dest='fasta', action='store', nargs='?',
-                        help='Input template sequences (*.fasta) for generating probes.',
+    parser.add_argument('-v', '--vcf', dest='vcf', action='store', nargs='?',
+                        help='Input vcf.gz (with .tbi) for phasing with shapeit (should be in your $PATH).',
                         metavar='FILE', required=True)
-    parser.add_argument('-l', '--len', dest='len', action='store', nargs='?',
-                        help='Length of probes.',
-                        metavar='STRING', required=True)
-    parser.add_argument('-s', '--step', dest='step', action='store', nargs='?',
-                        help='Step for generating probes.',
-                        metavar='STRING', required=True)
-    parser.add_argument('--min_freq', dest='min_freq', action='store', nargs='?',
-                        help='Minimum frequency for alleles to be considered (default: 0.05).'
-                             'Only use when --input is "bed".',
+    parser.add_argument("-r", "--ref", dest="ref", action="store", nargs='?',
+                        help="Input reference genome for subtracting sequences with BED.",
+                        metavar="FILE", required=True)
+    parser.add_argument('-b', '--bed', dest='bed', action='store', nargs='?',
+                        help='Genomic regions for subtracting sequences and phasing (BED).',
+                        metavar='FILE', required=True)
+    parser.add_argument('-t', '--thread', dest='thread', action='store', nargs='?',
+                        help='Number of threads (default: 1).',
+                        default=1, metavar='STRING')
+    parser.add_argument('-f', '--frequency', dest='freq', action='store', nargs='?',
+                        help='Minimum frequency of retained alleles (default: 0.05).',
                         default=0.05, metavar='STRING')
-    parser.add_argument('--sep', dest='sep', action='store', nargs='?',
-                        help='Separator for indicating allele num (default: _). '
-                             'Only use when --input is "seq".',
-                        default='_', metavar='STRING')
     parser.add_argument('-o', '--output', dest='output', action='store', nargs='?',
                         help='Output prefix.',
                         metavar='STRING', required=True)
     args = parser.parse_args()
 
-    fasta_dict = read_fasta(args.fasta)
+    BED = pybedtools.BedTool(args.bed)
+    results = []
+    BED_alleles_dict = {}
 
-    alleles_dict = process_fasta(fasta_dict, args.input, args.sep, float(args.min_freq))
-    print('Generating probe sequences ...', datetime.datetime.now())
-    probe_seq = get_probes_with_alleles(alleles_dict, 'eProbe', int(args.len), int(args.step))
+    tmp_dir = f'{args.output}_eProbe_temp'
+    make_temp_dir(tmp_dir)
+    print('Inferring haplotype sequences ...', datetime.datetime.now())
+    with multiprocessing.Pool(processes=int(args.thread)) as pool:
+        for bed_index in range(len(BED)):
+            results.append(pool.apply_async(lanch_process, args=(bed_index, args.bed, args.vcf, args.ref, tmp_dir)))
+        for result in results:
+            BED_alleles_dict[result.get()[0]] = result.get()[1]
 
-    output_probe_seq = {}
-    for gene, probes in probe_seq.items():
-        for num, probe in enumerate(probes, 1):
-            seq_id = f'{gene}.{num}'
-            output_probe_seq[seq_id] = probe
+    output_probe_dict = {}
+    for bed, alleles in BED_alleles_dict.items():
+        alt_count = 1
+        for allele, freq in alleles.items():
+            if freq >= float(args.freq):
+                output_probe_dict[f'{bed}_Allele{str(alt_count)}_Freq{round(freq, 4)}'] = allele
+                alt_count = alt_count + 1
 
-    print('Generating probe sequences completed.', datetime.datetime.now())
-    write_fasta(output_probe_seq, f'{args.output}.probe.fasta')
+    print('Inferring haplotype sequences completed.', datetime.datetime.now())
+    write_fasta(output_probe_dict, f'{args.output}.allele.fasta')
+    shutil.rmtree(tmp_dir)
